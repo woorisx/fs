2603ef0e1e0965ff7e6c6d5bced41a56fa5e7ba1
woorisx <suhwooree@gmail.com>
2026-04-07 22:03:48 +0900

    끝

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+$(document).ready(function(){
+    var menu_offset = $('nav').offset();
 
+    $(window).scroll(function(){
+        if($(document).scrollTop() > menu_offset.top){
+            $('nav').addClass('menuFixed');
+        }else{
+            $('nav').removeClass('menuFixed');
+        }
+    })
+});
 var backToTop = () =>{
     
     //scroll
